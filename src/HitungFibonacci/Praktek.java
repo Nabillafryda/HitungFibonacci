6f976eee3192ceff672e039fe335e4f7ed101a57
ASUS <ASUS@192.168.3.21>
2018-09-11 19:14:04 +0700

    Initial Commit

---
+++ src/HitungFibonacci/Praktek.java
@@ -0,0 +1,17 @@
+package HitungFibonacci;
+
+import java.util.Scanner;
+public class Praktek {
+    public static void tampilJudul(String identitas){
+        System.out.println("Identitas : " + identitas);
+
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21, ... dst.\n");        
+    }
+    
+     public static void main(String args){
+       String identitas = "Nabilla Fryda Israhani Nugraha / XR3 / 28";
+       tampilJudul(identitas);
+     
+     }
+}
