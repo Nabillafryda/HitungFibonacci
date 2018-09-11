806ad8da0622ce5631b6d8b0e5a2b36e7a063b96
ASUS <ASUS@192.168.3.21>
2018-09-11 19:26:38 +0700

    Tambah Hitung Fibonacci

--- src/HitungFibonacci/Praktek.java
+++ src/HitungFibonacci/Praktek.java
@@ -1,5 +1,6 @@
 package HitungFibonacci;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Praktek {
     public static void tampilJudul(String identitas){
@@ -17,11 +18,24 @@ public class Praktek {
         
         return n;        
     }
+    
+    private static BigInteger fibo(int n){
+        BigInteger[] hasil = new BigInteger[n];
         
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++){
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        return hasil[n-1];        
+    }   
+    
     public static void main(String args){
        String identitas = "Nabilla Fryda Israhani Nugraha / XR3 / 28";
        tampilJudul(identitas);
      
        int n = tampilInput();
+       BigInteger hasil = fibo(n);
     }
 }
