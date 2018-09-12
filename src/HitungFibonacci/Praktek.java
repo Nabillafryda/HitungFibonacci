78b4aecbb237384711b6c8dc662143ba045524a5
ASUS <ASUS@192.168.3.21>
2018-09-12 09:23:19 +0700

    Tambah Tampil Hasil

--- src/HitungFibonacci/Praktek.java
+++ src/HitungFibonacci/Praktek.java
@@ -29,13 +29,18 @@ public class Praktek {
             hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
         return hasil[n-1];        
-    }   
+    }  
+    
+    private static void tampilHasil(int n, BigInteger hasil){
+        System.out.println("Bilangan Fibonacci ke- " + n + " : " + hasil);
+    }
           
     public static void main(String args){
        String identitas = "Nabilla Fryda Israhani Nugraha / XR3 / 28";
        tampilJudul(identitas);
      
        int n = tampilInput();
-       BigInteger hasil = fibo(n);       
+       BigInteger hasil = fibo(n);     
+       tampilHasil(n, hasil);
     }
 }
